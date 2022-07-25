7906a2a4cbb4fcf75d9fef2b724320125d3736a6
Rostyslav Horytskyi <gorzer.rostyslav@gmail.com>
2022-07-25 23:04:38 +0300

    configure app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: "@mate-academy/eslint-config-react-typescript",
+  extends: ['@mate-academy/eslint-config-react-typescript', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
 };
